7d7fea13f21b084a7016f89cffcc29cc8661eed7
ShaziahQ <quadrishazia02@gmail.com>
2024-07-01 18:32:01 +0530

    Add files via upload

---
+++ src/components/index.jsx
@@ -0,0 +1,12 @@
+export { default as Navbar } from './Navbar/Navbar.jsx';
+export { default as Footer } from './Footer/Footer.jsx';
+export { default as Landing } from './Landing/Landing.jsx';
+export { default as About } from './About/About.jsx';
+export { default as Skills } from './Skills/Skills.jsx';
+export { default as Education } from './Education/Education.jsx';
+export { default as Experience } from './Experience/Experience.jsx';
+export { default as Contacts } from './Contacts/Contacts.jsx';
+export { default as Projects } from './Projects/Projects.jsx';
+export { default as SingleProject } from './Projects/SingleProject/SingleProject.jsx';
+
+export { default as BackToTop } from './BackToTop/BackToTop.jsx';
---
+++ src/data/aboutData.js
@@ -0,0 +1,6 @@
+export const aboutData = {
+    title: "Who I am",
+    description1: "Welcome to Smruti Ranjan Dhal's Digital Playground",
+    description2: "Greetings, fellow coders and curious minds! I am Smruti Ranjan Dhal, a passionate and results-driven software developer on a mission to turn innovative ideas into robust, scalable solutions. With a blend of creativity and technical prowess, I bring a unique perspective to the ever-evolving world of software development.",
+    image: 2
+}
